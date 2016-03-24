6d6395ff980e16dccfdecee0a522a14fc1ce6fca
gonzalo <gonzalo@ebizmarts.com>
2016-03-24 11:23:38 -0300

    closes #2

---
+++ registration.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * AutoResponder Magento Component
+ *
+ * @category Ebizmarts
+ * @package AutoResponder
+ * @author Ebizmarts Team <info@ebizmarts.com>
+ * @copyright Ebizmarts (http://ebizmarts.com)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @date: 3/24/16 11:15 AM
+ * @file: registration.php
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Ebizmarts_AutoResponder',
+    __DIR__
+);
